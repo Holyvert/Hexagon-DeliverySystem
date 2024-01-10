1a462d85186cab06452606d90ad1c02971659e93
Holyvert <96556635+Holyvert@users.noreply.github.com>
2024-01-09 19:54:29 -0500

    Create secretsConfig.ts

---
+++ src/environments/secretsConfig.ts
@@ -0,0 +1,9 @@
+//the Api key was removed for security reasons
+//this file is a template for a file named secrets.ts with the exact same structure
+//search on google for openCage and get an api key
+//https://opencagedata.com/
+// export const secrets = {
+//   openCage: {
+//     apiKey: '',
+//   },
+// };
